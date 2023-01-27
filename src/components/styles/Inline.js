b6b3eaa1af3a1bcdcabe86f343b7f002b0bfa623
Rajesh Anthari <antharir@gmail.com>
2023-01-27 22:19:25 +0530

    Day 4 learning forms and styling

---
+++ src/components/styles/Inline.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const heading = {
+  fontSize: '16px',
+  color: 'blue'
+}
+
+function Inline() {
+  return (
+    <div>
+      <h6 style={heading}>Inline Styling</h6>
+    </div>
+  )
+}
+
+export default Inline
